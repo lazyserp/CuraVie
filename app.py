bf2026cdf278549aa589a506b10e4dad1e236599
Aman Kumar Jha <Amanjha434@gmail.com>
2025-09-04 18:02:45 +0530

    fixed bugs

--- app.py
+++ app.py
@@ -28,6 +28,13 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
 
+
+@app.route("/")
+def home():
+    return "Welcome to CuraVie."
+
+
+
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
