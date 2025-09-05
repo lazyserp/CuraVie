d5a533ef3f0046d8353a7e84af2b5ba8037b4edc
anushka <anushka241105@gmail.com>
2025-09-05 23:23:07 +0530

    added login route

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_wtf.csrf import CSRFProtect
 from database import db
 from models import (
     User, Worker, HealthRecord,
-    HealthcareFacility, MedicalVisit, Vaccination
+    MedicalVisit, Vaccination
 )
 
 # Load environment variables
